44e906a61336dae190a1af3d67765fc98a959e53
Juan Felipe <juanfelipebedoyacardona@gmail.com>
2016-10-29 09:25:45 -0500

    Configurate the strategy for facebook

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports  = {
+	facebookStrategy: {
+		id: '1673228876301282',
+		secret: 'f8ccf51ae73864569cbd980fa0e3204a',
+		callbackUrl: 'http://localhost:8000/auth/facebook/callback',
+	}
+}
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cookieSession = require('cookie-session');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const graph = require('fbgraph');
 
+const config = require('./config/config.js');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -18,8 +20,28 @@ app.use(passport.session());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
+  console.log(JSON.stringify(config));
   res.render("index");
 });
+
+//Configurate passport
+	//Strategy
+passport.use(new FacebookStrategy({
+		clientID: config.facebookStrategy.id,
+		clientSecret: config.facebookStrategy.secret,
+		callbackUrl: config.facebookStrategy.callbackUrl
+	}, function(accessToken, refreshToken, profile, cb)
+	{
+		const user = { accessToken, profile}
+
+		cb(null, user);
+	}
+));
+	//How to store user in the session
+
+	//How to take the user of the session
+
+
 
 app.listen(8000, () => {
   console.log('Listening on port 8000');
