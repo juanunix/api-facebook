b7b5131a106a137c2dd750ca56ed0bb4eeea862f
Juan Bedoya <juanfelipebedoyacardona@gmail.com>
2016-11-04 08:06:11 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports  = {
 	facebookStrategy: {
-		id: '1673228876301282',
-		secret: 'f8ccf51ae73864569cbd980fa0e3204a',
+		id: 'API_CONSUMER_KEY',
+		secret: 'API_CONSUMER_SECRET',
 		callbackURL: 'http://localhost:8000/auth/facebook/callback',
 	}
 }
